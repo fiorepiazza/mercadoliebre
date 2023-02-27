65b1c7cb038c74be36f31a2e3e6c43990d784a51
fiorepiazza <fiorepiazza1997@gmail.com>
2023-02-27 16:37:54 -0300

    proyecto adaptado para render

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express()
 const path = require("path")
 
-app.listen(3030, ()=>console.log("Servidor corriendo en el puerto 3030"));
+const port = process.env.PORT || 3030;
+
+app.listen(port, ()=>console.log("Servidor corriendo en el puerto http://localhost:" + port));
 
 app.use(express.static(path.resolve("./public")))
 
